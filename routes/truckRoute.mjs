260b43d3b04f2bc16f603582d3a347460cb68e80
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-04-28 12:00:34 -0600

    Truck routes bugfix

--- routes/truckRoute.mjs
+++ routes/truckRoute.mjs
@@ -5,11 +5,10 @@ const router = express.Router();
 
 router.get('/',truckCTRL.getAllTrucks);
 router.get('/allIdle',truckCTRL.getAllIdleTrucks); //all idle before :licenceplate //order matters
+router.get('/allAvailable',truckCTRL.getAllAvailableTrucks); //all Available before :licenceplate //order matters
 router.get('/:licensePlate',truckCTRL.getTruckByLicensePlate);
 router.post('/',truckCTRL.postNewTruck);
 router.put('/:licensePlate',truckCTRL.updateTruckByLicensePlate);
 router.delete('/:licensePlate',truckCTRL.deleteTruckByLicensePlate);
-
-
 
 export default router
