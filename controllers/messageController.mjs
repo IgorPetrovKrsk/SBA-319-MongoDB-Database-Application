115c91c2385a7beb2b23dbc87098e8be50376a93
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-04-28 15:55:43 -0600

    messages by ID done

--- controllers/messageController.mjs
+++ controllers/messageController.mjs
@@ -12,25 +12,35 @@ async function getAllMessagesByStatusOrLicencePlate(req, res) {
         messages = await Messages.find({ status: req.params.statusOrLicencePlate });
         if (!messages || messages.length == 0) {
             res.json({ err: `Cannot find messages with license plate or status ${req.params.statusOrLicencePlate}` });
+        } else {
+            res.json(messages);
         }
+    } else {
+        res.json(messages);
     }
-    res.json(messages);
 }
 
 async function postNewMessage(req, res) {
     delete req.body.status; //date and status should be schemas default
-    delete req.body.date;  
+    delete req.body.date;
     const newMessage = await Messages.create(req.body);
     res.json(newMessage);
 }
 
-// async function updateTruckByLicensePlate(req, res) {
-//     const updatedTruck = await Trucks.findOneAndUpdate({ licensePlate: req.params.licensePlate }, req.body, { new: true });
-//     if (!updatedTruck) {
-//         res.json({ err: `Cannot find truck with license plate ${req.params.licensePlate}` })
-//     }
-//     res.json(updatedTruck);
-// }
+async function updateMessageById(req, res) {
+
+    req.body.date = Date.now();
+    let updatedMessage = await Messages.findById(req.params.id);
+    if (!updatedMessage) {
+        res.json({ err: `Cannot find message with id ${req.params.id}` });
+    } else if (updatedMessage.status != 'Pending') {
+        res.json({ err: `Cannot change message witch in not 'Pending' id:${req.params.id}` });
+    } else {
+        Object.assign(updatedMessage, req.body);
+        await updatedMessage.save();
+        res.json(updatedMessage);
+    }
+}
 
 // async function deleteTruckByLicensePlate(req, res) {
 //     const deletedTruck = await Trucks.findOneAndDelete({ licensePlate: req.params.licensePlate });
@@ -40,5 +50,5 @@ async function postNewMessage(req, res) {
 //     res.json(deletedTruck);
 // }
 
-export default { getAllMessages, getAllMessagesByStatusOrLicencePlate,postNewMessage }
+export default { getAllMessages, getAllMessagesByStatusOrLicencePlate, postNewMessage, updateMessageById }
 
--- routes/messageRoute.mjs
+++ routes/messageRoute.mjs
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/',messageCTRL.getAllMessages);
 router.get('/:statusOrLicencePlate',messageCTRL.getAllMessagesByStatusOrLicencePlate);
 router.post('/',messageCTRL.postNewMessage);
-// router.put('/:licensePlate',truckCTRL.updateTruckByLicensePlate);
+router.put('/:id',messageCTRL.updateMessageById);
 // router.delete('/:licensePlate',truckCTRL.deleteTruckByLicensePlate);
 
 export default router
